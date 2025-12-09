728f4d0c278e403f864d63d7a48b5eca3dd692ec
mine0059 <onojefemuemine@gmail.com>
2025-12-09 21:17:32 +0100

    add: auth_widget export

--- lib/auth_sdk.dart
+++ lib/auth_sdk.dart
@@ -4,3 +4,4 @@ export 'src/models/auth_user.dart';
 export 'src/exceptions//exceptions.dart';
 export 'src/repository/auth_repository.dart';
 export 'src/state/auth_cubit/auth_cubit.dart';
+export 'src/ui/auth_widget.dart';
